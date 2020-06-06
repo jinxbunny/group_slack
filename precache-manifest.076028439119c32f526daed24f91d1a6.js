e560ccfc4023a7905a992f0dec0aa85873ebdb3c
jinxbunny <info@jmkit.com>
2020-06-06 18:55:38 +0100

    Updates

---
+++ precache-manifest.076028439119c32f526daed24f91d1a6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3f12c9dd3aeb1d5742a45dc784c7ad",
+    "url": "/group_slack/index.html"
+  },
+  {
+    "revision": "63c67e739ef3401db681",
+    "url": "/group_slack/static/css/main.2adc62ab.chunk.css"
+  },
+  {
+    "revision": "603be6601fd7f0a1393b",
+    "url": "/group_slack/static/js/2.e63c1e99.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/group_slack/static/js/2.e63c1e99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63c67e739ef3401db681",
+    "url": "/group_slack/static/js/main.6f2ae547.chunk.js"
+  },
+  {
+    "revision": "52310412bded8f323bb7",
+    "url": "/group_slack/static/js/runtime-main.1727a694.js"
+  },
+  {
+    "revision": "9e31161108ad69cc75264c71274e8a58",
+    "url": "/group_slack/static/media/CircularStd-Book.9e311611.ttf"
+  },
+  {
+    "revision": "e4ba63a6345c96f3cb813ef00340beda",
+    "url": "/group_slack/static/media/CircularStd-Medium.e4ba63a6.ttf"
+  },
+  {
+    "revision": "4fa8e1afb9bbf3d2f3a20c166b6c8132",
+    "url": "/group_slack/static/media/textbox_left.4fa8e1af.png"
+  },
+  {
+    "revision": "f22b826a29f61be778676dfb66cfbd47",
+    "url": "/group_slack/static/media/textbox_right.f22b826a.png"
+  },
+  {
+    "revision": "d7ed1c40763095040741a7d51fffc33d",
+    "url": "/group_slack/static/media/user1.d7ed1c40.png"
+  },
+  {
+    "revision": "87db3ab4fef40c5b6d4d6a64e8ae18c6",
+    "url": "/group_slack/static/media/user2.87db3ab4.png"
+  },
+  {
+    "revision": "821df8bf8a7d3466dfa7771f6ffc0bb6",
+    "url": "/group_slack/static/media/user3.821df8bf.png"
+  }
+]);
