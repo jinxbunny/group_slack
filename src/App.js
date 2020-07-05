797fcbb1e47eb8e11e01ed3d10cb724e51de011a
Robert <info@jmkit.com>
2020-07-05 20:41:12 +0100

    uptodate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Toolbar from './components/Toolbar';
 import MainFeed1 from './components/MainFeed1';
@@ -10,31 +10,31 @@ import ag from './img/user2.png';
 import rm from './img/user3.png';
 
 
-class App extends Component{
+class App extends Component {
   state = {
     username: "Paddy",
     userpic: pa,
     time: '',
     chats: [
-      {pic: rm, user: "Robert Marsh", text: "Here's some sample messages!", time: "4:00PM"},
-      {pic: ag, user: "Andy Green", text: "How about another sample message", time: "4:01PM"},
-      {pic: pa, user: "Paddy", text: "Looks good..", time: "4:02PM"},
-      {pic: rm, user: "Robert Marsh", text: "Ok thank should be enough!", time: "4:03PM"},
-  ]
+      { pic: rm, user: "Robert Marsh", text: "Here's some sample messages!", time: "4:00PM" },
+      { pic: ag, user: "Andy Green", text: "How about another sample message", time: "4:01PM" },
+      { pic: pa, user: "Paddy", text: "Looks good..", time: "4:02PM" },
+      { pic: rm, user: "Robert Marsh", text: "Ok thank should be enough!", time: "4:03PM" },
+    ]
   }
 
   clickedNew = (newText) => {
     var date, TimeType, hour, minutes, fullTime;
     date = new Date();
-    hour = date.getHours(); 
+    hour = date.getHours();
     //Set AM or PM
-    if(hour <= 11){TimeType = 'AM';}
-    else{TimeType = 'PM';}
-    if( hour > 12 ){hour = hour - 12;}
+    if (hour <= 11) { TimeType = 'AM'; }
+    else { TimeType = 'PM'; }
+    if (hour > 12) { hour = hour - 12; }
     //If the hour shows as 0 convert to 12
-    if( hour === 0 ){hour = 12;} 
+    if (hour === 0) { hour = 12; }
     minutes = date.getMinutes();
-    if(minutes < 10){minutes = '0' + minutes.toString();}
+    if (minutes < 10) { minutes = '0' + minutes.toString(); }
     //Display the time like 4:00PM
     fullTime = hour.toString() + ':' + minutes.toString() + TimeType.toString();
     //Set the elements of the posted message
@@ -42,10 +42,10 @@ class App extends Component{
     let newUsername = this.state.username
     let curTime = fullTime;
     this.setState({
-      chats: [...this.state.chats,  {pic: newPic, user: newUsername, text: newText, time: curTime}],
+      chats: [...this.state.chats, { pic: newPic, user: newUsername, text: newText, time: curTime }],
     })
-}
-  
+  }
+
   switchUserRM = () => {
     this.setState({
       username: "Robert Marsh",
@@ -69,20 +69,19 @@ class App extends Component{
   }
 
 
-  render () {
+  render() {
     return (
       <React.Fragment>
         <BrowserRouter>
-        <div id="toolbar"><Toolbar/></div>
-        <div id="mainSection">
-        <div id="users"><Users 
-            switchRM={this.switchUserRM} switchAG={this.switchUserAG} switchPA={this.switchUserPA}/></div>
-          <div id="channels"><Channels/></div>
-          <MainFeed1 chats={this.state.chats} clickedNew={this.clickedNew} />
-          {/* <Switch>
-            <Route exact path = "/" render ={() => <MainFeed1 chats={this.state.chats} clickedNew={this.clickedNew} />} />
-          </Switch> */}
-        </div>
+          <div id="toolbar"><Toolbar /></div>
+          <div id="mainSection">
+            <div id="users"><Users
+              switchRM={this.switchUserRM} switchAG={this.switchUserAG} switchPA={this.switchUserPA} /></div>
+            <div id="channels"><Channels /></div>
+            <Switch>
+              <Route exact path="/" render={() => <MainFeed1 chats={this.state.chats} clickedNew={this.clickedNew} />} />
+            </Switch>
+          </div>
         </BrowserRouter>
       </React.Fragment>
     )
